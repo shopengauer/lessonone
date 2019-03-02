242e6a3999734b431cb7ea529abe1469649ec711
root <localducks@gmail.com>
2019-03-02 22:22:07 +0300

    ListView

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.brown,
+        primarySwatch: Colors.lightBlue,
       ),
       home: MyHomePage(title: 'Lesson one'),
     );
@@ -26,49 +26,75 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+
+
+
 
   void _incrementCounter() {
     setState(() {
-      _counter++;
+
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: ListView(children: <Widget>[
-        DrawerHeader(decoration: BoxDecoration(color: Colors.black12),
-            child: Text("Head",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 30,
-                    fontStyle: FontStyle.normal))),
-        ListTile(
-          title: Text("First"),
-          onTap: () {},
-        ),
-        ListTile(
-          title: Text("Second"),
-          onTap: () {},
-        ),
-        ListTile(
-          title: Text("Third"),
-          onTap: () {},
-        ),
-      ]),
-      appBar: AppBar(
-        leading: new IconButton(
-            alignment: AlignmentDirectional.centerStart,
-            tooltip: "inc",
-            icon: new Icon(Icons.sync),
-            onPressed: _incrementCounter),
+      drawer: Container(
+
+        //decoration: BoxDecoration(color: Colors.lightBlueAccent, border: Border.all(style: BorderStyle.solid)),
+        child: ListView(children: <Widget>[
+          DrawerHeader(decoration: BoxDecoration(color: Colors.white, border: Border.all(style: BorderStyle.solid)),
+              child: Text("Портфель", style: getDrawerTextStyle())),
+          Container(
+            decoration: BoxDecoration(color: Colors.white, border: Border.all(style: BorderStyle.solid)),
+            child: ListTile(
+              leading: getImage(),
+              title: Text("AT&T", style: getDrawerTextStyle()),
+              onTap: () {},
+              trailing: Text('106\$'),
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(color: Colors.white, border: Border.all(style: BorderStyle.solid)),
+            child: ListTile(
+              leading: getVloImage(),
+              title: Text("Valero Energy", style: getDrawerTextStyle()),
+              onTap: () {},
+            ),
+          ),
+          ListTile(
+            title: Text("Third"),
+            onTap: () {},
+          ),
+        ]),
       ),
+//      appBar: AppBar(
+//        leading: new IconButton(
+//            alignment: AlignmentDirectional.centerStart,
+//            tooltip: "inc",
+//            icon: new Icon(Icons.sync),
+//            onPressed: _incrementCounter),
+//      ),
       persistentFooterButtons: <Widget>[
         FlatButton(onPressed: () {}, child: new Icon(Icons.free_breakfast)),
         FlatButton(onPressed: () {}, child: new Icon(Icons.airport_shuttle)),
         FlatButton(onPressed: () {}, child: new Icon(Icons.local_gas_station))
       ],
+      body: Center(child: Text("Main screen")),
     );
   }
 }
+
+
+class IconWid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Icon(Icons.local_gas_station);
+  }
+}
+
+getOilIcon() => Icon(Icons.aspect_ratio, color: Colors.black87);
+getImage()=> Image.network('https://storage.googleapis.com/iex/api/logos/T.png', scale: 3,);
+getVloImage()=> Image.network('https://storage.googleapis.com/iex/api/logos/VLO.png', scale: 3,);
+getDrawerTextStyle() => TextStyle(color: Colors.black87, fontSize: 20);
+
